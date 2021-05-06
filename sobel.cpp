f8f142aa77bceb2a938e4fa1ebd16d86fdb85eca
Filip Olsen <filip.kenya@gmail.com>
2021-05-06 19:40:14 +0200

    beginning recognizing barcodes

--- sobel.cpp
+++ sobel.cpp
@@ -14,7 +14,7 @@ using namespace std;
 using Clock=std::chrono::high_resolution_clock;
 
 void ShowManyImages(string title, int nArgs, ...);
-void ProcessMatImage(cv::Mat& img);
+Mat ProcessMatImage(cv::Mat &img);
 
 int main(int argc, char* argv[])
 {
@@ -31,12 +31,13 @@ int main(int argc, char* argv[])
     imgs.push_back(img4);
 
     int counter = 0;
-    for (cv::Mat& img : imgs)
+    for (cv::Mat &img : imgs)
     {
         auto t1 = Clock::now();
 
         /* Process mat under chrono timed */
-        ProcessMatImage(img);
+        cv::Mat raw;
+        raw = ProcessMatImage(img);
 
         auto t2 = Clock::now();
         std::cout << "Delta t2-t1: " 
@@ -44,28 +45,29 @@ int main(int argc, char* argv[])
                   << " nanoseconds" << std::endl;
 
         counter++;
-        cv::namedWindow("img"+counter, WINDOW_NORMAL);
-        cv::imshow("img"+counter, img);
+
+        cv::namedWindow(to_string(counter), WINDOW_NORMAL);
+        cv::imshow(to_string(counter), img);
+
+        cv::namedWindow("raw" + to_string(counter), WINDOW_NORMAL);
+        cv::imshow("raw" + to_string(counter), raw);
     }
-
-    const String namedWindow = "Scharr derived gradient";
-
-    //ShowManyImages(namedWindow, 4, imgs[0], imgs[1], imgs[2], imgs[3]);
-
-    //cv::namedWindow(namedWindow, WINDOW_NORMAL);
-    //cv::imshow(namedWindow, img);
 
     cv::waitKey(0);
 
     return 0;
 }
 
-void ProcessMatImage(cv::Mat& img) {
+bool sortContourArea (vector<Point> i, vector<Point> j) { return (contourArea(i) > contourArea(j)); }
+
+Mat ProcessMatImage(cv::Mat &img) {
+    //RawImg
+    cv::Mat raw = img;
     // Convert RGB Mat to GRAY
     cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
 
     // Scale down
-    double scaleFactor = 0.2;
+    double scaleFactor = 0.3;
     cv::resize(img, img, cv::Size(), scaleFactor, scaleFactor);
 
     // Calculate image gradient
@@ -95,110 +97,143 @@ void ProcessMatImage(cv::Mat& img) {
     cv::blur(img, img, Size(15,15));
 
     // Threshold gradient image
-    cv::threshold(img, img, 110, 255, THRESH_BINARY);
+    cv::threshold(img, img, 115, 255, THRESH_BINARY);
+
+    // Fill larger gaps between lines
+    Mat kernel;
+    kernel = cv::getStructuringElement(MORPH_RECT, Size(21, 7));
+
+    cv::morphologyEx(img, img, MORPH_CLOSE, kernel);
+
+    // Remove noisy artifacting
+    cv::erode(img, img, kernel, Point(-1,-1), 2, BORDER_CONSTANT, morphologyDefaultBorderValue());
+    cv::dilate(img, img, kernel, Point(-1,-1), 5, BORDER_CONSTANT, morphologyDefaultBorderValue());
+
+    // Find and Create largest bounding box (contours)
+    cv::Mat ogScale;
+    cv::resize(img, ogScale, raw.size(), 0, 0);
+
+    vector<vector<Point>> imgContours;
+    vector<Vec4i> hierarchy;
+    cv::findContours( ogScale, imgContours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE );
+
+    std::sort(imgContours.begin(), imgContours.end(), sortContourArea);
+
+    Point2f vtx[4];
+    RotatedRect rect = cv::minAreaRect(imgContours[0]);
+    rect.points(vtx);
+
+    for (int i = 0; i < 4; i++)
+    {
+        line(raw, vtx[i], vtx[(i+1)%4], Scalar(0,255,0), 10, LINE_AA);
+    }
+
+    return raw;
 }
 
 
 
-void ShowManyImages(string title, int nArgs, ...) {
-int size;
-int i;
-int m, n;
-int x, y;
+void ShowManyImages(string title, int nArgs, ...)
+{
+    int size;
+    int i;
+    int m, n;
+    int x, y;
 
-// w - Maximum number of images in a row
-// h - Maximum number of images in a column
-int w, h;
+    // w - Maximum number of images in a row
+    // h - Maximum number of images in a column
+    int w, h;
 
-// scale - How much we have to resize the image
-float scale;
-int max;
+    // scale - How much we have to resize the image
+    float scale;
+    int max;
 
-// If the number of arguments is lesser than 0 or greater than 12
-// return without displaying
-if(nArgs <= 0) {
-    printf("Number of arguments too small....\n");
-    return;
-}
-else if(nArgs > 14) {
-    printf("Number of arguments too large, can only handle maximally 12 images at a time ...\n");
-    return;
-}
-// Determine the size of the image,
-// and the number of rows/cols
-// from number of arguments
-else if (nArgs == 1) {
-    w = h = 1;
-    size = 300;
-}
-else if (nArgs == 2) {
-    w = 2; h = 1;
-    size = 300;
-}
-else if (nArgs == 3 || nArgs == 4) {
-    w = 2; h = 2;
-    size = 300;
-}
-else if (nArgs == 5 || nArgs == 6) {
-    w = 3; h = 2;
-    size = 200;
-}
-else if (nArgs == 7 || nArgs == 8) {
-    w = 4; h = 2;
-    size = 200;
-}
-else {
-    w = 4; h = 3;
-    size = 150;
-}
-
-// Create a new 3 channel image
-Mat DispImage = Mat::zeros(Size(100 + size*w, 60 + size*h), CV_8UC3);
-
-// Used to get the arguments passed
-va_list args;
-va_start(args, nArgs);
-
-// Loop for nArgs number of arguments
-for (i = 0, m = 20, n = 20; i < nArgs; i++, m += (20 + size)) {
-    // Get the Pointer to the IplImage
-    Mat img = va_arg(args, Mat);
-
-    // Check whether it is NULL or not
-    // If it is NULL, release the image, and return
-    if(img.empty()) {
-        printf("Invalid arguments");
+    // If the number of arguments is lesser than 0 or greater than 12
+    // return without displaying
+    if(nArgs <= 0) {
+        printf("Number of arguments too small....\n");
         return;
     }
-
-    // Find the width and height of the image
-    x = img.cols;
-    y = img.rows;
-
-    // Find whether height or width is greater in order to resize the image
-    max = (x > y)? x: y;
-
-    // Find the scaling factor to resize the image
-    scale = (float) ( (float) max / size );
-
-    // Used to Align the images
-    if( i % w == 0 && m!= 20) {
-        m = 20;
-        n+= 20 + size;
+    else if(nArgs > 14) {
+        printf("Number of arguments too large, can only handle maximally 12 images at a time ...\n");
+        return;
+    }
+    // Determine the size of the image,
+    // and the number of rows/cols
+    // from number of arguments
+    else if (nArgs == 1) {
+        w = h = 1;
+        size = 300;
+    }
+    else if (nArgs == 2) {
+        w = 2; h = 1;
+        size = 300;
+    }
+    else if (nArgs == 3 || nArgs == 4) {
+        w = 2; h = 2;
+        size = 300;
+    }
+    else if (nArgs == 5 || nArgs == 6) {
+        w = 3; h = 2;
+        size = 200;
+    }
+    else if (nArgs == 7 || nArgs == 8) {
+        w = 4; h = 2;
+        size = 200;
+    }
+    else {
+        w = 4; h = 3;
+        size = 150;
     }
 
-    // Set the image ROI to display the current image
-    // Resize the input image and copy the it to the Single Big Image
-    Rect ROI(m, n, (int)( x/scale ), (int)( y/scale ));
-    Mat temp; resize(img,temp, Size(ROI.width, ROI.height));
-    temp.copyTo(DispImage(ROI));
-}
+    // Create a new 3 channel image
+    Mat DispImage = Mat::zeros(Size(100 + size*w, 60 + size*h), CV_8UC3);
 
-// Create a new window, and show the Single Big Image
-namedWindow( title, 1 );
-imshow( title, DispImage);
-waitKey();
+    // Used to get the arguments passed
+    va_list args;
+    va_start(args, nArgs);
 
-// End the number of arguments
-va_end(args);
+    // Loop for nArgs number of arguments
+    for (i = 0, m = 20, n = 20; i < nArgs; i++, m += (20 + size)) {
+        // Get the Pointer to the IplImage
+        Mat img = va_arg(args, Mat);
+
+        // Check whether it is NULL or not
+        // If it is NULL, release the image, and return
+        if(img.empty()) {
+            printf("Invalid arguments");
+            return;
+        }
+
+        // Find the width and height of the image
+        x = img.cols;
+        y = img.rows;
+
+        // Find whether height or width is greater in order to resize the image
+        max = (x > y)? x: y;
+
+        // Find the scaling factor to resize the image
+        scale = (float) ( (float) max / size );
+
+        // Used to Align the images
+        if( i % w == 0 && m!= 20) {
+            m = 20;
+            n+= 20 + size;
+        }
+
+        // Set the image ROI to display the current image
+        // Resize the input image and copy the it to the Single Big Image
+        Rect ROI(m, n, (int)( x/scale ), (int)( y/scale ));
+        Mat temp;
+        resize(img,temp, Size(ROI.width, ROI.height));
+        temp.copyTo(DispImage(ROI));
+    }
+
+    // Create a new window, and show the Single Big Image
+    namedWindow( title, 1 );
+    imshow( title, DispImage);
+    waitKey();
+
+    // End the number of arguments
+    va_end(args);
 }
